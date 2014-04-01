1554fa591c9133ad038392297d7ddbee8f2a8d0c
tomaszdurka <tomasz@durka.pl>
2014-04-02 00:13:58 +0200

    Use initd and monit engines in main engine

--- lib/foreman_debian/engine.rb
+++ lib/foreman_debian/engine.rb
@@ -1,15 +1,18 @@
 module ForemanDebian
   class Engine
 
-    attr_accessor :name, :user
-
-    def initialize(name, user)
-      @name = name
+    def initialize(app, user)
+      @app = app
       @user = user
+      @initd_engine = Engine::Initd.new(@app, @user)
+      @monit_engine = Engine::Monit.new(@app, @user)
     end
 
     def install(jobs)
-      puts jobs
+      jobs.each do |name, command|
+        @initd_engine.install(name, command)
+        @monit_engine.install(name, command)
+      end
     end
 
     def uninstall
