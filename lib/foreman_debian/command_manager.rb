82d3e9dd7ce494b8e54627101baeb152dd439272
tomaszdurka <tomasz@durka.pl>
2014-04-08 09:47:38 +0200

    Add start/stop subcommands

--- lib/foreman_debian/command_manager.rb
+++ lib/foreman_debian/command_manager.rb
@@ -3,5 +3,7 @@ module ForemanDebian
 
     subcommand 'install', 'Install', Command::Install
     subcommand 'uninstall', 'Uninstall', Command::Uninstall
+    subcommand 'start', 'Start', Command::Start
+    subcommand 'stop', 'Stop', Command::Stop
   end
 end
