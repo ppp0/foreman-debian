7eeb2978a34fc7be702789fe1d4fe0afe8f4b755
tomaszdurka <tomasz@durka.pl>
2014-04-09 15:58:08 +0200

    Add second arg for create script spec, use literal

--- spec/foreman_debian/initd/engine_spec.rb
+++ spec/foreman_debian/initd/engine_spec.rb
@@ -6,13 +6,13 @@ describe ForemanDebian::Initd::Engine, :fakefs do
   let(:engine) { ForemanDebian::Initd::Engine.new('app', 'app-user') }
 
   it 'creates script' do
-    script = engine.create_script('foo', 'foo arg1')
+    script = engine.create_script('foo', 'foo arg1 arg2')
 
     script.path.to_s.should == '/etc/init.d/app-foo'
     script.name.should == 'app-foo'
     script.description.should == 'app-foo'
     script.user.should == 'app-user'
-    script.arguments.should == ['arg1']
+    script.arguments.should == %w(arg1 arg2)
     script.pidfile.to_s.should == '/var/run/app-foo/app-foo.pid'
   end
 
