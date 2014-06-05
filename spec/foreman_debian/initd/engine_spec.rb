d22bd8725d7b31c36789e77bc012c8c422473c98
tomaszdurka <tomasz@durka.pl>
2014-06-05 14:32:34 +0200

    Replace should with expect

--- spec/foreman_debian/initd/engine_spec.rb
+++ spec/foreman_debian/initd/engine_spec.rb
@@ -16,29 +16,29 @@ describe ForemanDebian::Initd::Engine, :fakefs do
   it 'creates script' do
     script = engine.create_script('foo', 'foo arg1 arg2')
 
-    script.path.to_s.should == '/etc/init.d/app-foo'
-    script.name.should == 'app-foo'
-    script.description.should == 'app-foo'
-    script.user.should == 'app-user'
-    script.arguments.should == %w(arg1 arg2)
-    script.pidfile.to_s.should == '/var/run/app-foo/app-foo.pid'
+    expect(script.path.to_s) == '/etc/init.d/app-foo'
+    expect(script.name) == 'app-foo'
+    expect(script.description) == 'app-foo'
+    expect(script.user) == 'app-user'
+    expect(script.arguments) == %w(arg1 arg2)
+    expect(script.pidfile.to_s) == '/var/run/app-foo/app-foo.pid'
   end
 
   it 'installs script' do
     script = engine.create_script('foo', 'bar arg1 arg2')
     engine.install(script)
-    File.read('/etc/init.d/app-foo').should == spec_resource('initd_script/app-foo')
+    expect(File.read('/etc/init.d/app-foo')) == spec_resource('initd_script/app-foo')
   end
 
   it 'starts script' do
     script_path = Pathname.new('/etc/init.d/foo')
     engine.start_file(script_path)
-    engine.commands_run.should == ['/etc/init.d/foo start', 'update-rc.d foo defaults']
+    expect(engine.commands_run) == ['/etc/init.d/foo start', 'update-rc.d foo defaults']
   end
 
   it 'stops script' do
     script_path = Pathname.new('/etc/init.d/foo')
     engine.stop_file(script_path)
-    engine.commands_run.should == ['/etc/init.d/foo stop', 'update-rc.d foo remove']
+    expect(engine.commands_run) == ['/etc/init.d/foo stop', 'update-rc.d foo remove']
   end
 end
