d9469984688ff8022490cbc5299d1c6e13c76cf0
tomaszdurka <tomasz@durka.pl>
2014-06-05 14:43:12 +0200

    Add spec for engine.job_concurrency

---
+++ spec/foreman_debian/engine_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe ForemanDebian::Engine, :fakefs do
+
+  let(:engine) { ForemanDebian::Engine.new('app', 'app-user') }
+
+  it 'job concurrency detects if job should be enabled' do
+    {
+        {'all' => 1} => 1,
+        {'all' => 0, 'foo' => 1} => 1,
+        {'all' => 1, 'foo' => 0} => 0,
+        {} => 0,
+        {'bar' => 1} => 0,
+    }.each do |concurrency, expected|
+      expect(engine.job_concurrency(concurrency, 'foo')).to be == expected
+    end
+  end
+end
